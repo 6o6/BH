8dc9f2891d832f65d3bfded1ec12d5f89ff7e4f8
Jennifer Ling <jenniferchingling@gmail.com>
2017-04-01 07:06:12 -0700

    Routific request working

--- DriverApp/js/main.js
+++ DriverApp/js/main.js
@@ -14,6 +14,52 @@ app.config(function($routeProvider) {
 
 app.controller("mapController", function($scope, $location, $http) {
 
+var queryParams = {
+  "visits": {
+    "order_1": {
+      "location": {
+        "name": "6800 Cambie",
+        "lat": 49.227107,
+        "lng": -123.1163085
+      }
+    },
+    "order_2": {
+      "location": {
+        "name": "3780 Arbutus",
+        "lat": 49.2474624,
+        "lng": -123.1532338
+      }
+    },
+    "order_3": {
+      "location": {
+        "name": "800 Robson",
+        "lat": 49.2819229,
+        "lng": -123.1211844
+      }
+    }
+  },
+  "fleet": {
+    "vehicle_1": {
+      "start_location": {
+        "id": "depot",
+        "name": "800 Kingsway",
+        "lat": 49.2553636,
+        "lng": -123.0873365
+      }
+    }
+  }
+};
+
+var creds = {
+  headers:{ 'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OGRmMDcxM2VkNzE0MWFlMGE3ZWI5OWQiLCJpYXQiOjE0OTEwNTQ3NTJ9.rL0viwCPcei2rcj7xf8W5hDyQYxsBuc5cxfeyUdFDL8'}
+}
+
+$http.post("https://api.routific.com/v1/vrp", queryParams, creds)
+        .then(function (data) {
+          console.log(data.status);
+        }, function (error) {
+          console.log(error.toString());
+        });
 });
 
 app.controller("acceptController", function($scope, $location) {
