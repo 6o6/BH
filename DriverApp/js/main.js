76398e52ccab2d0061f3690f0cff3a27277b69af
Jennifer Ling <jenniferchingling@gmail.com>
2017-04-01 01:38:01 -0700

    Confirmation page for customer, driver

--- DriverApp/js/main.js
+++ DriverApp/js/main.js
@@ -6,15 +6,22 @@ app.config(function($routeProvider) {
         templateUrl: "partials/map.html",
         controller: "mapController"
     })
-    .when("/confirm", {
+    .when("/accept", {
         templateUrl: "partials/acceptRequest.html",
         controller: "acceptController"
     });
 });
 
 app.controller("mapController", function($scope) {
-
+  
 });
 
-app.controller("acceptController", function($scope) {
+app.controller("acceptController", function($scope, $location) {
+  $scope.customerID = "#111111";
+  $scope.customerName = "Jane Doe";
+  $scope.customerAddress = "2222 Broadway St.";
+  $scope.customerPhone = "604-111-1111";
+  $scope.travelDistance = "3km";
+  $scope.timeFrame = "Within 3 hours";
+  
 });
